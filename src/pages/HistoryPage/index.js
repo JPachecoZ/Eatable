6ec8315db905dc21e413fc5444859a3504865c9a
Javier Pacheco <jepach@gmail.com>
2022-04-07 13:48:06 -0500

    Finished History Page UI

--- src/pages/HistoryPage/index.js
+++ src/pages/HistoryPage/index.js
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 import TotalCart from "../../components/TotalCart";
 import Text from "../../components/Text";
 import Button from "../../components/Button";
-import { IoIosArrowBack } from 'react-icons/io'
+import { IoIosArrowBack, IoIosArrowDown, IoIosArrowUp } from 'react-icons/io'
 
 const Container = styled.section`
   max-width: 25.94rem;
@@ -32,14 +32,6 @@ const Title = styled.div`
   width: 100%;
 `
 
-const DetailsHeading = styled.div`
-  display:flex;
-  margin-top: 1.875rem;
-  margin-bottom: 1.25rem;
-  justify-content: space-between;
-  width: 100%;
-`
-
 const DataDetails = styled.div`
   background-color: var(--bg-2);
   box-shadow: 0px 10px 40px rgba(0, 0, 0, 0.03);
@@ -47,6 +39,7 @@ const DataDetails = styled.div`
   padding: 1.5625rem 3.25rem 1.25rem 1.875rem;
   display: flex;
   flex-direction: column;
+  width: 19.6875rem;
   gap:0.625rem;
 `
 
@@ -57,7 +50,17 @@ const LineDiv = styled.div`
   width: 100%;
 `
 
+const TotalContainer = styled.div`
+display: flex;
+justify-content: space-between;
+width: 100%;
+`
+
+
 export default function HistoryPage(){
+
+  const active = true;
+
   return(
     <Container>
       <div>
@@ -66,23 +69,45 @@ export default function HistoryPage(){
         <Text bold size="l">History</Text>
         <div></div>
       </Title>
-      <Text bold size="xl">Delivery</Text>
-      <DetailsHeading>
-        <Text bold size="m">Address details</Text>
-        <Text bold size="s" color="var(--accent-color)">change</Text>
-      </DetailsHeading>
       <DataDetails>
-        <Text bold size="m">Margarita Flores</Text>
+        <Text size="m">Wed, Mar 20, 2021</Text>
+        <TotalContainer>
+          <Text size="s">3 items</Text>
+          <Text color="var(--accent-color)" size="s">$64.90</Text>
+        </TotalContainer>
+        <TotalContainer>
+          <div></div>
+          <IoIosArrowDown/>
+        </TotalContainer>
+      </DataDetails>
+
+      <DataDetails>
+        <Text size="m">Wed, Mar 20, 2021</Text>
+        <TotalContainer>
+          <Text size="s">2 items</Text>
+          <Text color="var(--accent-color)" size="s">$97.90</Text>
+        </TotalContainer>
+
+        <Text bold size="m">Order</Text>
+        <TotalContainer>
+          <Text size="s">5 - Veggie tomato mix</Text>
+          <Text size="s">$75.45</Text>
+        </TotalContainer>
+        <TotalContainer>
+          <Text size="s">1 - Fishwith mix orange...</Text>
+          <Text size="s">$12.45</Text>
+        </TotalContainer>
         <LineDiv/>
-        <Text size="m">Calle Rosales 123, urb El Jardin</Text>
-        <LineDiv/>
-        <Text size="m">987654321</Text>
+
+        <Text bold size="m">Delivery</Text>
+        <Text size="s">Calle Rosales 123, urb El Jardin</Text>
+
+        <TotalContainer>
+          <div></div>
+          {active ? <IoIosArrowUp/> : <IoIosArrowDown/>}
+        </TotalContainer>
       </DataDetails>
       </div>
-      <Footer>
-        <TotalCart total="$27.90"/>
-        <Button fullWidth>Complete Order</Button>
-      </Footer>
     </Container>
   )
 }
